eed25bd477d82588be7aeb2527a9ca0afe3f8bcc
Anton Neururer <31255932+jupiter-online@users.noreply.github.com>
2017-09-25 19:24:23 +0200

    Fixed bug in examples file

--- examples.py
+++ examples.py
@@ -18,7 +18,7 @@ print(fo(-1000000, typ='bin', sep=' '))
 
 print(fo(-1000000, sep='__'))
 print(fo(-1000000, typ='hex', sep='__'))
-print(fo(-1000000, typ='bin', sep='__ '))
+print(fo(-1000000, typ='bin', sep='__'))
 
 print(fo(-9999999, sep='__', dis=0))
 print(fo(-9999999, typ='hex', sep='__', dis=4))
